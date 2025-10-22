195e57466b73d5de05b602e74410025a973a5cbd
Spyder <114872079+spydernet3@users.noreply.github.com>
2025-10-22 21:49:23 +0530

    Refactor notificationclick event listener

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 // Listen for notification click event
-self.addEventListener('notificationclick', event => {
+self.addEventListener('notificationclick', function(event) {
   event.notification.close();
-  event.waitUntil(clients.openWindow('https://spydernet3.github.io/Nothing-Reminder/'));
+  event.waitUntil(
+    clients.openWindow('https://spydernet3.github.io/Nothing-Reminder/')
+  );
 });
